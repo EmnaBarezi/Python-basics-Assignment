a51b9bfc4a4b8db627d2ab5669a773faeff1a1c8
Tetaaline <a.teta2@alustudent>
2025-10-30 07:38:11 -0700

    Teta Aline

--- greetings.py
+++ greetings.py
@@ -1,8 +1,8 @@
 # creation of  function show_intro
-def show_intro():
-    print("=" * 30)
-    print("Welcome to the User Info Program")
-    print("=" * 30)
+        def show_intro():
+            print("=" * 30)
+                print("Welcome to the User Info Program")
+                    print("=" * 30)
 
-def show_exit_message():
-    print("Thank you for using the program. Goodbye!")
+                    def show_exit_message():
+                        print("Thank you for using the program. Goodbye!")                    
